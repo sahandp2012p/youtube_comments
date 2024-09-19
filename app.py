a8a2d0acf6b8263fc3464f8644cc276a1bf02b0a
sahandp2012p <spourjavad2012@gmail.com>
2024-09-19 11:57:29 +0330

    fixed bug

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from get_sentiments import get_sentiment
 from downloads import download
-
 download()
+from get_sentiments import get_sentiment
+
 
 id = input('Enter id: ')
 print((((get_sentiment(id)[0]--1)*(10-1))/(1--1))+1)
