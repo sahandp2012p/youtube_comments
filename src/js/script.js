fdf023f8047a99c23b5fcbb3e1379170d6a845c5
sahandp2012p <spourjavad2012@gmail.com>
2024-09-23 18:36:53 +0330

    Removed extra code

--- src/js/script.js
+++ src/js/script.js
@@ -6,20 +6,6 @@ const form = document.getElementById("score")
 const formSubmit = (event) =>{
 	event.preventDefault()
 	const formData = new FormData(event.target)
-// 	fetch('http://localhost:8000/', {
-// 		method: "POST",
-// 		body: JSON.stringify({
-// 			id: formData.get('id')
-// 		})
-// 	})
-//   .then(response => response.json())
-//   .then((data) => {
-//   	if (data['detail']) {
-//   		document.querySelector('p').innerText = data['detail']
-//   	} else {
-//   		document.querySelector('p').innerText =  `${data['score']}/10 ${data['emoji']} Out of ${data['comments']} comments`
-//   	}
-//   })
 	axios.post('http://localhost:8000', {
 		id: formData.get('id')
 	}).then(response => {
