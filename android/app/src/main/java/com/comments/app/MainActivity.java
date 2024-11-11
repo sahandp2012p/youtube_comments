9021f4c29903bf9700a2af294eb0d9a876fe14c4
sahandp2012p <spourjavad2012@gmail.com>
2024-11-11 12:12:23 -0800

    Made responsive

--- android/app/src/main/java/com/comments/app/MainActivity.java
+++ android/app/src/main/java/com/comments/app/MainActivity.java
@@ -1,5 +1,17 @@
 package com.comments.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Disable scrolling in the WebView
+        WebView webView = (WebView) this.bridge.getWebView();
+        webView.setVerticalScrollBarEnabled(true);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setOnTouchListener((v, event) -> event.getAction() == 2); // Prevents scrolling by intercepting touch events
+    }
+}
