66edf820c75a64661376f8f09e917ecdca909a7a
sahandp2012p <spourjavad2012@gmail.com>
2024-12-15 10:51:56 -0800

    fixed router bug

--- src/router.js
+++ src/router.js
@@ -4,13 +4,19 @@ import HomeView from "./pages/HomeView.vue";
 import OneComment from "./pages/OneComment.vue";
 
 const routes = [
-  { path: "/*", component: HomeView },
+  { path: "/", component: HomeView },
   { path: "/onecomment", component: OneComment },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/' 
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.replace({ path: '*', redirect: '/' })
 
 export default router;
